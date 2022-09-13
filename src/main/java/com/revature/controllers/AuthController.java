7c8027f8750127b8b92bf03ce0e10f983a9a3e94
Rei <umbrarei@gmail.com>
2022-09-13 15:48:10 -0400

    added isAdmin and isActive to user

--- src/main/java/com/revature/controllers/AuthController.java
+++ src/main/java/com/revature/controllers/AuthController.java
@@ -48,7 +48,9 @@ public class AuthController {
                 registerRequest.getEmail(),
                 registerRequest.getPassword(),
                 registerRequest.getFirstName(),
-                registerRequest.getLastName());
+                registerRequest.getLastName(),
+                false,
+                true);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(created));
     }
