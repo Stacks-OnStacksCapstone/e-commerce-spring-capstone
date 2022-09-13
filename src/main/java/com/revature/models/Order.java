597f362634a16cfd015d7e9b2f5848ff75327a71
Paul Mork <paul.a.mork@gmail.com>
2022-09-13 13:52:32 -0700

    Added Order model

---
+++ src/main/java/com/revature/models/Order.java
@@ -0,0 +1,26 @@
+package com.revature.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User userId;
+    private String paymentId;
+    private Date orderDate;
+    private String shipmentAddress;
+    private boolean orderFulfilled;
+}
