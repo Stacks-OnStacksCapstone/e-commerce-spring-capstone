611d378d445cb4f8955345e4a1d25cb54a031ed6
Elham-AT <110053378+Elham-AT@users.noreply.github.com>
2022-09-15 11:22:41 -0500

    check for password in updateuserrequest added

--- src/main/java/com/revature/dtos/UpdateUserRequest.java
+++ src/main/java/com/revature/dtos/UpdateUserRequest.java
@@ -4,12 +4,16 @@ package com.revature.dtos;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Pattern;
+
 @Data
 @NoArgsConstructor
 public class UpdateUserRequest {
 
     private String firstName;
     private String lastName;
+
+    @Pattern(message = "The password syntax is invalid! please try again.",regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")
     private String password;
 
 
