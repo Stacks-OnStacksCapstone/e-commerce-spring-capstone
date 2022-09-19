dca343a1199c94c535c5ea693d9ba191a3be5660
Paul Mork <paul.a.mork@gmail.com>
2022-09-19 09:40:45 -0700

    Added check for matching user when viewing order details

--- src/main/java/com/revature/controllers/OrderDetailController.java
+++ src/main/java/com/revature/controllers/OrderDetailController.java
@@ -5,11 +5,16 @@ import com.revature.dtos.OrderDetailRequest;
 import com.revature.dtos.OrderDetailResponse;
 import com.revature.models.Order;
 import com.revature.models.OrderDetail;
+import com.revature.models.User;
 import com.revature.services.OrderDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+import javax.xml.ws.Response;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,15 +29,13 @@ public class OrderDetailController {
     public OrderDetailController(OrderDetailService orderDetailService) {
         this.orderDetailService = orderDetailService;
     }
-    @Authorized
-    @GetMapping
-    public ResponseEntity<List<OrderDetail>> findAllOrderDetails(){return ResponseEntity.ok(orderDetailService.findAll());}
 
     @Authorized
     @GetMapping("/{id}")
-    public ResponseEntity<OrderDetail> findById(@PathVariable("id") int id){
+    public ResponseEntity<OrderDetail> findById(@PathVariable("id") int id, HttpSession httpSession){
+        User user = (User) httpSession.getAttribute("user");
         Optional<OrderDetail> optional = orderDetailService.findById(id);
-        if(!optional.isPresent()){
+        if(!optional.isPresent() || (optional.get().getOrderId().getUserId().getId() != user.getId())){
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(optional.get());
@@ -40,8 +43,17 @@ public class OrderDetailController {
 
     @Authorized
     @GetMapping("/order/{id}")
-    public ResponseEntity<List<OrderDetailResponse>> findAllByOrderId(@PathVariable("id") int id) {
+    public ResponseEntity<List<OrderDetailResponse>> findAllByOrderId(@PathVariable("id") int id, HttpSession httpSession) {
+        User user = (User) httpSession.getAttribute("user");
         List<OrderDetailResponse> orderDetailResponses = orderDetailService.findAllOrderDetailsByOrder(id);
+        orderDetailResponses = (ArrayList) orderDetailResponses;
+        if (orderDetailResponses.size() == 0) {
+            return ResponseEntity.notFound().build();
+        }
+        Order order = orderDetailService.findById( orderDetailResponses.get(0).getId()).get().getOrderId();
+        if (user.getId() != order.getUserId().getId()) {
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(orderDetailResponses);
     }
 
