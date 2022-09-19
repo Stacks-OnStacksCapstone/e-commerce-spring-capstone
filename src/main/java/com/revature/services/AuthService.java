bddfa6be652c054a447516227c664b24f324c8e7
calebabeck2020 <calebabeck@gmail.com>
2022-09-19 17:48:03 -0400

    SAVEPOINT: Spring Security and Spring Mail successfully implemented, sends emails from noreplycongo.revature@gmail.com. Ready to implement tokens for resetting password

--- src/main/java/com/revature/services/AuthService.java
+++ src/main/java/com/revature/services/AuthService.java
@@ -6,18 +6,22 @@ import com.revature.exceptions.InvalidUserInputException;
 import com.revature.exceptions.ResourceNotFoundException;
 import com.revature.models.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.security.auth.callback.ConfirmationCallback;
 import java.util.Optional;
 
 @Service
 public class AuthService {
 
     private final UserService userService;
+    private final SendEmailService sendEmailService;
 
-    public AuthService(UserService userService) {
+    public AuthService(UserService userService, SendEmailService sendEmailService) {
         this.userService = userService;
+        this.sendEmailService = sendEmailService;
     }
 
     public Optional<User> findByCredentials(String email, String password) {
@@ -27,10 +31,20 @@ public class AuthService {
     @Transactional
     public void resetPassword(UpdateUserRequest updateUserRequest) {
         User user = userService.findByEmail(updateUserRequest.getEmail()).orElseThrow(ResourceNotFoundException::new);
-        if(updateUserRequest.getPassword() == null || updateUserRequest.getPassword().equals("")) {
-            throw new InvalidUserInputException();
-        }
-        userService.update(updateUserRequest, user);
+
+        String randomPassword = ":)";
+        String to = user.getEmail();
+        String subject = "Reset your Congo Password";
+        String text = "Your password has been changed! \n New Password: " + randomPassword;
+
+        sendEmailService.sendEmail(to,subject,text);
+
+
+
+//        if(updateUserRequest.getPassword() == null || updateUserRequest.getPassword().equals("")) {
+//            throw new InvalidUserInputException();
+//        }
+//        userService.update(updateUserRequest, user);
     }
     public User register(User user) {
         return userService.save(user);
