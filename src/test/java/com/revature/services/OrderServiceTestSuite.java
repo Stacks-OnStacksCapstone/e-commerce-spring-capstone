53a1f958a160d58996e7506534c60dab6775e970
[TanStanPlan] <[tanmanstanplan@gmail.com]>
2022-09-20 17:26:03 -0400

    Created and finished test_findById_returnOrderResponse_givenValidId

--- src/test/java/com/revature/services/OrderServiceTestSuite.java
+++ src/test/java/com/revature/services/OrderServiceTestSuite.java
@@ -2,6 +2,7 @@ package com.revature.services;
 
 import com.revature.dtos.CreateOrderRequest;
 import com.revature.dtos.OrderResponse;
+import com.revature.exceptions.ResourceNotFoundException;
 import com.revature.models.Order;
 import com.revature.models.Payment;
 import com.revature.models.User;
@@ -9,10 +10,12 @@ import com.revature.repositories.OrderRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 public class OrderServiceTestSuite {
@@ -64,6 +67,13 @@ public class OrderServiceTestSuite {
         User validUser = spy(new User(1, "valid", "valid", "valid", "valid", true, true));
         Payment validPayment = spy(new Payment("1", "valid", new Date(2000,12,12), "12345", (float) 0.01, validUser));
         Order validOrder = spy(new Order(1, validUser, validPayment, new Date(2000,12,12), "valid"));
+
+        when(mockOrderRepository.findById(1)).thenReturn(Optional.of(validOrder));
+
+        Order actualOrder = sut.findById(1).orElseThrow(ResourceNotFoundException::new);
+
+        Assertions.assertInstanceOf(Order.class, actualOrder);
+        verify(mockOrderRepository, times(1));
     }
 
 }
