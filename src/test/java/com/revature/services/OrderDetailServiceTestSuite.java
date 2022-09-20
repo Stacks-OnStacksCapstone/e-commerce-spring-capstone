e4c0157f0158ebe83f7d707fae67354cb20641a0
Byron <byronfedele@gmail.com>
2022-09-20 15:17:39 -0400

    Fixed test_createOrderDetail_returnOrderDetailResponse_givenValidCreateOrderDetailRequest()

--- src/test/java/com/revature/services/OrderDetailServiceTestSuite.java
+++ src/test/java/com/revature/services/OrderDetailServiceTestSuite.java
@@ -1,29 +1,38 @@
 package com.revature.services;
 
+import com.revature.dtos.CreateOrderRequest;
 import com.revature.dtos.OrderDetailRequest;
 import com.revature.dtos.OrderDetailResponse;
-import com.revature.dtos.PaymentResponse;
 import com.revature.models.*;
 import com.revature.repositories.OrderDetailRepository;
+import com.revature.repositories.OrderRepository;
+import com.revature.repositories.PaymentRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 
-import com.revature.services.ProductService;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 
 import java.sql.Date;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 
 public class OrderDetailServiceTestSuite {
     private static OrderDetailService sut;
     private static OrderDetailRepository orderDetailRepository;
+    private static OrderRepository orderRepository;
+    private static PaymentRepository paymentRepository;
     private static ProductService productService;
     private static OrderService orderService;
 
+    private static PaymentService paymentService;
+
+
     @BeforeAll
     static void init() {
+
+        orderRepository = mock(OrderRepository.class);
         orderDetailRepository = mock(OrderDetailRepository.class);
         productService = mock(ProductService.class);
         orderService = mock(OrderService.class);
@@ -35,12 +44,16 @@ public class OrderDetailServiceTestSuite {
         Payment validPayment = spy(new Payment("1", "valid", new Date(2000,12,12), "12345", (float) 0.01, validUser));
         Product validProduct = spy(new Product(1,1,1,"valid","valid","valid",true));
         Order validOrder = spy(new Order(1, validUser, validPayment, new Date(2000,12,12), "valid"));
-        OrderDetail validOrderDetail = spy(new OrderDetail(1,validOrder,validProduct,1));
         OrderDetailRequest orderDetailRequest = spy(new OrderDetailRequest(1,1,1));
-        //orderDetailRepository.save(validOrderDetail);
-        OrderDetailResponse validOrderDetailResponse=sut.createOrderDetail(orderDetailRequest);
 
-        //Assertions.assertFalse(orderDetailRepository.existsById(validOrderDetail.getId()));
+        when(orderService.findById(validOrder.getId())).thenReturn(Optional.of(validOrder));
+        when(productService.findById(orderDetailRequest.getProductId())).thenReturn(Optional.of(validProduct));
+        doReturn(new OrderDetail(orderDetailRequest, validOrder, validProduct)).when(orderDetailRepository).save(any(OrderDetail.class));
+
+        OrderDetailResponse validOrderDetailResponse=sut.createOrderDetail(orderDetailRequest);
         Assertions.assertInstanceOf(OrderDetailResponse.class, validOrderDetailResponse);
+
+        verify(orderDetailRepository, times(1));
+
     }
 }
