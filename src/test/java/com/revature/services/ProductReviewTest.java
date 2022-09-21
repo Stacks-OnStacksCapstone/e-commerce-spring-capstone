33a5ea9247d0b95bca6fc32bf2a222fa485ac294
Rei <umbrarei@gmail.com>
2022-09-21 12:07:34 -0400

    Added findAll/findAll by ID tests for Reviews

--- src/test/java/com/revature/services/ProductReviewTest.java
+++ src/test/java/com/revature/services/ProductReviewTest.java
@@ -20,8 +20,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -55,7 +58,9 @@ class ProductReviewTest {
 
     private ProductReviewResponse productReviewResponse1;
 
-    private Product productValid1;
+    private Product productValid1, productValid2, productValid3;
+
+    private List<Product> productList1;
 
     private User userValid1;
 
@@ -72,14 +77,25 @@ class ProductReviewTest {
         productValid1 = new Product(
                 999,1,20,"Valid","Valid","Valid",true);
 
+        productValid2 = new Product(
+                999,1,20,"Valid","Valid","Valid",true);
+
+        productValid3 = new Product(
+                999,1,20,"Valid","Valid","Valid",true);
+
         userValid1 = new User(999,"Valid","Valid","Valid","Valid",true,true);
 
         productReview1 = new ProductReview(
                 999,5,"Valid", productValid1, userValid1);
 
         productReview2=new ProductReview(
-                0,0,"", new Product(), new User());
+                0,0,"", productValid1, userValid1);
 
+
+        productList1 = new ArrayList<>();
+        productList1.add(productValid1);
+        productList1.add(productValid2);
+        productList1.add(productValid3);
 
         productReviewRequest1= new ProductReviewRequest(
                 999,5,"Valid",999);
@@ -100,7 +116,6 @@ class ProductReviewTest {
 
 
     @Test
-    @DisplayName("check valid input")
     public void testSaveProductReview_givenValidInput(){
 
         when(productReviewRepository.save(any())).thenReturn(productReview1);
@@ -111,7 +126,6 @@ class ProductReviewTest {
     }
 
     @Test
-    @DisplayName("check invalid input")
     public void testSaveProductReview_givenInvalidInput(){
 
         ProductReview productReviewTemp =  productReviewService.save(productReviewRequest2, userValid1);
@@ -119,7 +133,29 @@ class ProductReviewTest {
     }
 
 
+    @Test
+    public void testFindAllProductReviews() {
+        when(productReviewRepository.findAll()).thenReturn(new ArrayList<>());
 
+        List<ProductReviewResponse> products = productReviewService.findAll();
+        verify(productReviewRepository,times(1)).findAll();
+    }
 
+    @Test
+    public void testFindAllProductReviewsBySpecificProduct() {
+        ArrayList<ProductReview> arr = new ArrayList<>();
+        arr.add(productReview1);
+        arr.add(productReview2);
 
+        when(productReviewRepository.findallByProductId(eq(1))).thenReturn(new ArrayList<>());
+        when(productReviewRepository.findallByProductId(eq(2))).thenReturn(arr);
+
+        List<ProductReviewResponse> products = productReviewService.findByProductId(1);
+        verify(productReviewRepository,times(1)).findallByProductId(1);
+        assertEquals(0, products.size());
+
+        products = productReviewService.findByProductId(2);
+        verify(productReviewRepository,times(1)).findallByProductId(2);
+        assertEquals(2, products.size());
+    }
 }
