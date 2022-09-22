2948941d8fecce63d346056d91191566f3f168d5
Elham-AT <110053378+Elham-AT@users.noreply.github.com>
2022-09-22 12:57:17 -0500

    merge conflict resolve

--- src/main/java/com/revature/controllers/OrderDetailController.java
+++ src/main/java/com/revature/controllers/OrderDetailController.java
@@ -10,11 +10,7 @@ import com.revature.services.OrderDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpSession;
-import javax.xml.ws.Response;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
