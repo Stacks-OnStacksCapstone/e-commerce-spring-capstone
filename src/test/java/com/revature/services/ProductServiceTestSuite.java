7a671be2f0d836ccece40735e3a211ede77b6e1b
kdang1928 <kimdang@ucsb.edu>
2022-09-22 12:18:09 -0700

    Finished Unit Tests for Product Services

--- src/test/java/com/revature/services/ProductServiceTestSuite.java
+++ src/test/java/com/revature/services/ProductServiceTestSuite.java
@@ -1,5 +1,6 @@
 package com.revature.services;
 
+import com.revature.dtos.ProductInfo;
 import com.revature.models.Product;
 import com.revature.repositories.ProductRepository;
 import org.junit.jupiter.api.Assertions;
@@ -21,24 +22,6 @@ public class ProductServiceTestSuite {
     public static void testPrep(){
         mockProductRepository = mock(ProductRepository.class);
         sut = new ProductService(mockProductRepository);
-    }
-
-    @Test
-    public void test_save_returnsNewProduct_givenValidProduct(){
-        Product newProduct = new Product(777,
-        12,
-        99.99,
-        "This new product is very expensive",
-        "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
-        "New Valid Product",
-        true);
-
-        when(mockProductRepository.save(newProduct)).thenReturn(newProduct);
-        newProduct.setId(2);
-        Product returnedProduct = sut.save(newProduct);
-
-        Assertions.assertInstanceOf(Product.class, returnedProduct);
-        verify(mockProductRepository, times(1));
     }
 
     @Test
@@ -72,7 +55,7 @@ public class ProductServiceTestSuite {
                 "Headphones",
                 true);
 
-        when(mockProductRepository.findById(eq(1))).thenReturn(Optional.of(newProduct));
+        when(mockProductRepository.findActiveById(eq(1))).thenReturn(Optional.of(newProduct));
 
         Product returnedProduct = sut.findById(1).get();
 
@@ -110,5 +93,77 @@ public class ProductServiceTestSuite {
         }
 
         verify(mockProductRepository, times(1));
+    }
+
+    @Test
+    public void test_save_returnsNewProduct_givenValidProduct(){
+        Product newProduct = new Product(777,
+                12,
+                99.99,
+                "This new product is very expensive",
+                "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+                "New Valid Product",
+                true);
+
+        when(mockProductRepository.save(newProduct)).thenReturn(newProduct);
+        newProduct.setId(2);
+        Product returnedProduct = sut.save(newProduct);
+
+        Assertions.assertInstanceOf(Product.class, returnedProduct);
+        verify(mockProductRepository, times(1));
+    }
+
+    @Test
+    public void test_saveAll_returnsNewProduct_givenValidProduct(){
+        List<Product> products = new ArrayList<>();
+        List<ProductInfo> productInfos = new ArrayList<>();
+        Product bag = new Product(3,
+                20,
+                2.50,
+                "A reusable shopping bag",
+                "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+                "Shopping Bag",
+                true);
+
+        Product baseballCap = new Product( 4,
+                20,
+                10.00,
+                "A fancy cap for a fancy person",
+                "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+                "Baseball Cap",
+                true);
+
+        products.add(bag);
+        products.add(baseballCap);
+        productInfos.add(new ProductInfo(3, 20));
+        productInfos.add(new ProductInfo(4, 20));
+
+        when(mockProductRepository.saveAll(products)).thenReturn(products);
+
+        List<Product> returnedProducts = sut.saveAll(products, productInfos);
+
+        for(Product p : returnedProducts){
+            Assertions.assertInstanceOf(Product.class, p);
+        }
+
+        verify(mockProductRepository, times(1));
+    }
+
+    @Test
+    public void test_delete_givenValidInt(){
+        Product newProduct = new Product(1,
+                10,
+                20.00,
+                "A nice pair of headphones",
+                "https://i.insider.com/54eb437f6bb3f7697f85da71?width=1000&format=jpeg&auto=webp",
+                "Headphones",
+                true);
+
+        when(mockProductRepository.findActiveById(eq(1))).thenReturn(Optional.of(newProduct));
+        when(mockProductRepository.save(newProduct)).thenReturn(newProduct);
+
+        sut.delete(1);
+
+        verify(mockProductRepository, times(2));
     }
 }
