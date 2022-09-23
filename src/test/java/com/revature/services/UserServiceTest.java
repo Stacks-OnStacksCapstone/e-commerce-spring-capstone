4f437ec444c83ad2ef3b5dd9c165c687ad5a2979
Elham-AT <110053378+Elham-AT@users.noreply.github.com>
2022-09-23 09:55:48 -0500

    UserService tests added

--- src/test/java/com/revature/services/UserServiceTest.java
+++ src/test/java/com/revature/services/UserServiceTest.java
@@ -11,6 +11,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.servlet.http.HttpSession;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+
 
 import static org.mockito.Mockito.*;
 
@@ -27,11 +35,14 @@ class UserServiceTest {
     private RegisterRequest registerRequest;
     private UpdateUserRequest updateUserRequest;
 
+
+
     @BeforeEach
     public void setUp(){
         user1 = new User(1, "Valid", "Valid", "Valid", "Valid", false, true,null);
         registerRequest = new RegisterRequest("Valid", "Valid", "Valid", "Valid");
         updateUserRequest = new UpdateUserRequest("Valid", "Valid", "Valid","Valid");
+
     }
 
     @AfterEach
@@ -51,12 +62,85 @@ class UserServiceTest {
         verify(userRepository, times(1)).save(any());
     }
 
-//    @Test
-//    @DisplayName("Test should pass when updating a user given valid input")
-//    public void testUpdateUser_givenValidInput(){
+    // JUnit test for getEmployeeById method
+    @DisplayName("JUnit test for findUserById method")
+    @Test
+    public void givenUserId_whenGetUserById_thenReturnUserObject(){
+        // given
+        given(userRepository.findById(1)).willReturn(Optional.of(user1));
+
+        // when
+        User savedUSer = userService.findUserById(user1.getId());
+
+        // then
+        assertThat(savedUSer).isNotNull();
+
+    }
+
+    // JUnit test for update method
+    @DisplayName("JUnit test for update method")
+    @Test
+    public void givenUserObject_whenUpdateUser_thenReturnUpdatedUser(){
+        // given - precondition or setup
+        given(userRepository.findById(user1.getId())).willReturn(Optional.ofNullable(user1));
+
+
+        // when -  action or the behaviour that we are going test
+        userService.update(updateUserRequest, user1);
+
+        // then - verify the output
+        assertThat(user1.getFirstName()).isEqualTo(updateUserRequest.getFirstName());
+        assertThat(user1.getLastName()).isEqualTo(updateUserRequest.getLastName());
+        assertThat(user1.getPassword()).isEqualTo(updateUserRequest.getPassword());
+
+    }
+
+    @DisplayName("JUnit test for deactivate method")
+    @Test
+    public void givenUserObject_whenDeactivateUser_thenReturnDeactivatedUser(){
+        // given - precondition or setup
+        given(userRepository.findById(1)).willReturn(Optional.ofNullable(user1));
+
+
+        // when -  action or the behaviour that we are going test
+        userService.deactivate(user1);
+
+        // then - verify the output
+        verify(userRepository, times(1)).deactivateUser(user1.getId());
+
+    }
+
+    @DisplayName("JUnit test for save method")
+    @Test
+    public void givenUserObject_whenSaveUser_thenReturnUserObject(){
+        // given - precondition or setup
+        given(userRepository.save(user1)).willReturn(user1);
+
+        System.out.println(userRepository);
+        System.out.println(userService);
+
+        // when -  action or the behaviour that we are going test
+        User savedUser = userService.save(user1);
+
+        System.out.println(savedUser);
+        // then - verify the output
+        assertThat(savedUser).isNotNull();
+    }
+
+    // JUnit test for getEmployeeById method
+    @DisplayName("JUnit test for findByCredentials method")
+    @Test
+    public void givenUserEmailAndPassword_whenGetUserEmailAndPassword_thenReturnUserObject(){
+        // given
+        given(userRepository.findByEmailAndPassword(user1.getEmail(),user1.getPassword())).willReturn(Optional.of(user1));
+
+        // when
+        Optional<User> savedUser = userService.findByCredentials("Valid","Valid");
+
+        // then
+        assertThat(savedUser).isNotNull();
+
+    }
+
 //
-//        when(userRepository.findById().thenReturn(user1);
-//        userService.update(updateUserRequest, user1);
-//        verify(userRepository, times(1)).save(any());
-//    }
 }
