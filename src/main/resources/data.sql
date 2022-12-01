30cfb05b90288211f9e449e779d9d91430ecc049
Jeremy F McKay <jeremyfmckay@gmail.com>
2022-12-01 11:58:39 -0800

    Added more products

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -31,7 +31,7 @@ INSERT INTO product (id, quantity, price, description, image, name, is_active) V
     10.00,
     'A fancy cap for a fancy person',
     'https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png',
-    'Baseball Cap',
+    'Hat',
     true
 ),
 (
@@ -42,23 +42,49 @@ INSERT INTO product (id, quantity, price, description, image, name, is_active) V
     'https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png',
     'Coat',
     true
-);
-
-INSERT INTO users (id, email, password, first_name, last_name, is_active, is_admin) VALUES
+),
 (
-    1,
-    'testuser@gmail.com',
-    'password',
-    'Test',
-    'User',
-    true,
+    6,
+    10,
+    120.00,
+    'A beater set of headphones',
+    'https://www.beatsbydre.com/content/dam/beats-support/global/images/product-hero/set-up-and-use-studio-wireless.jpg.large.2x.jpg',
+    'Headphones',
     true
 ),
-(   2,
-    'notadmin@gmail.com',
-    'password',
-    'not',
-    'admin',
-    true,
-    false
+(
+    7,
+    5,
+    99.99,
+    'A nicer MeeShirt',
+    'https://cdn-images.threadless.com/threadless-media/artist_shops/shops/myMckay/products/1793362/original-1604545416-bfb4e7e3c4fb93eae97d92920bf7f3d2.png?v=3&d=eyJvbmx5X21ldGEiOiBmYWxzZSwgImZvcmNlIjogZmFsc2UsICJvcHMiOiBbWyJ0cmltc2NyaXB0IiwgWzEyMDAuMCwgMTM3MS40Mjg1NzE0Mjg1NzEzXSwge31dLCBbImVuY29kZSIsIFsiLnBuZyJdLCB7ImRwaSI6IDMwMH1dLCBbInJlc2l6ZSIsIFs3MzBdLCB7fV0sIFsib3ZlcmxheSIsIFsidGhyZWFkbGVzcy1tZWRpYS9hcnRpc3Rfc2hvcHMvb3ZlcmxheXMvYWYyODdiMjg0YWE0NmM3ZGQxYWMxYzNkMjBkMjk2NTQvZnJvbnQtMTU2OTUxODMzMS1hOTMzYTI3NjZiZGE2ODFjNWQzZWE2Mjc2NDU4MmRhZi5wbmciXSwgeyJ5IjogMzY4LCAieCI6IDY0MCwgImJhY2tncm91bmQiOiAiZGVkZWRlIn1dLCBbInJlc2l6ZSIsIFtdLCB7IndpZHRoIjogMzUwLCAic3R5bGUiOiAiQ1JPUCIsICJoZWlnaHQiOiAzNTB9XSwgWyJlbmNvZGUiLCBbImpwZyIsIDg1XSwge31dXX0=',
+    'TeeShirt',
+    true
+),
+(
+    8,
+    20,
+    12.50,
+    'A reusable surgical mask',
+    'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2022_29/3512377/screen_shot_2021-10-13_at_2-00-29_pm.png',
+    'Mask',
+    true
+),
+(
+    9,
+    1,
+    1000.00,
+    'A fancier hat for a fancier person',
+    'https://cdn.shopify.com/s/files/1/2624/7744/products/ReversibleRen-RedSilver-Leather-TopHat-AmericanHatMakers-Men-STUD_900x900.jpg?v=1668713281',
+    'Hat',
+    true
+),
+(
+    10,
+    3,
+    1.99,
+    'A niceish coat',
+    'https://media.bergdorfgoodman.com/f_auto,q_auto:good,ar_5:7,c_fill,dpr_1.0,w_720/01/bg_4370145_100664_m',
+    'Coat',
+    true
 );
