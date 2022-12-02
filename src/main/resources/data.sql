1acc83e9a6d7e44a3ab19fa2c08b2d79ec0fb561
David Hanzlik <david420@revature.net>
2022-12-02 11:05:39 -0600

    added configuration to enable h2 console, created data.sql with some prefilled data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,58 @@
+INSERT INTO users (email, password, first_name, last_name, is_active, is_admin) VALUES
+(
+    'admin@example.com',
+    'guest',
+    'Admin First',
+    'Admin Last',
+    true,
+    true
+),
+(
+    'test@example.com',
+    'guest',
+    'Test First',
+    'Test Last',
+    true,
+    false
+);
+
+INSERT INTO product (quantity, price, description, image, name, is_active) VALUES (
+    10,
+    20.00,
+    'A nice pair of headphones',
+    'https://i.insider.com/54eb437f6bb3f7697f85da71?width=1000&format=jpeg&auto=webp',
+    'Headphones',
+    true
+),
+(
+    5,
+    45.00,
+    'A nice TeeShirt',
+    'https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png',
+    'TeeShirt',
+    true
+),
+(
+    20,
+    2.50,
+    'A reusable shopping bag',
+    'https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png',
+    'Shopping Bag',
+    true
+),
+(
+    20,
+    10.00,
+    'A fancy cap for a fancy person',
+    'https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png',
+    'Baseball Cap',
+    true
+),
+(
+    3,
+    80.00,
+    'A nice coat',
+    'https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png',
+    'Coat',
+    true
+);
