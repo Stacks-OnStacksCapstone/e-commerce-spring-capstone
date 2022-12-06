37cb241fdce6bbf618304beb6b842a92d899da87
Brithnie Estime <brithnieestime@Brithnies-MacBook-Pro.local>
2022-12-06 17:51:25 -0500

    updateProfile stepsImplementation skeleto code. renamed UserProfile class to UserProfilePage. update runner class. added testing dependencies to pom.xml.

--- src/test/java/com/revature/stepsImplementation/UpdateProfile.java
+++ src/test/java/com/revature/stepsImplementation/UpdateProfile.java
@@ -1,4 +1,55 @@
 package com.revature.stepsImplementation;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class UpdateProfile {
+
+    @Given("User is logged in")
+    public void user_is_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User clicks on the Profile link")
+    public void user_clicks_on_the_profile_link() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User is navigated to the Profile page")
+    public void user_is_navigated_to_the_profile_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User is able to see two MUI boxes {string} and {string}")
+    public void user_is_able_to_see_two_mui_boxes_and(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User types in {string} in the first name input field")
+    public void user_types_in_in_the_first_name_input_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("types in {string} in the last name input field")
+    public void types_in_in_the_last_name_input_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("types in {string} in the password input field")
+    public void types_in_in_the_password_input_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("clicks on the Update button")
+    public void clicks_on_the_update_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("An Alert for successfully updated profile says {string}")
+    public void an_alert_for_successfully_updated_profile_says(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
