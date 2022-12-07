624df933d47173ecdb20483cc3776d098f81d505
cmiller241 <99040885+cmiller241@users.noreply.github.com>
2022-12-07 17:20:06 -0500

    Functioning MockMVC code for Product Inventory

--- src/test/java/com/revature/mock/TestProductController.java
+++ src/test/java/com/revature/mock/TestProductController.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -19,9 +18,10 @@ import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(controllers= ProductController.class)
-public class WebTestProductController {
+public class TestProductController {
 
     @MockBean
     private ProductService productservice;
@@ -30,19 +30,21 @@ public class WebTestProductController {
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("Should List All Posts When Making Get Request to Endpoint - /api/product/")
+    @DisplayName("Should List All Products - /api/product/")
     public void shouldListAllProducts() throws Exception {
 
-        List<Product> product = new ArrayList<Product>;
-        product.add(new Product(1,1,50,"Poop","img/src.gif","Poop",true));
+        List<Product> product = new ArrayList<Product>();
+        product.add(new Product(1,1,50,"test","img/src.gif","test",true));
         when(productservice.findAll()).thenReturn(product);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/product/"))
             .andExpect(MockMvcResultMatchers.status().is(200))
             .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$[0].description").value("test"))
+                .andExpect(jsonPath("$[0].image").value("img/src.gif"))
+                .andExpect(jsonPath("$[0].name").value("test"))
                 .andDo(print())
                 .andReturn();
-
     }
 
 }
