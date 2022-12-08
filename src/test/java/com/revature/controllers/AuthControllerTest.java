e17c4530c3070719597a8bfe1e7cafbac17ecb18
Jeremy F McKay <jeremyfmckay@gmail.com>
2022-12-07 19:28:36 -0800

    Finally got /login endpoint working with MockMVC and H2.

---
+++ src/test/java/com/revature/controllers/AuthControllerTest.java
@@ -0,0 +1,79 @@
+package com.revature.controllers;
+
+
+import com.revature.ECommerceApplication;
+import com.revature.models.User;
+import com.revature.services.AuthService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+//@EnableJpaRepositories(basePackages = {"com.revature.repositories"})
+//@DataJpaTest
+@SpringBootTest(classes = ECommerceApplication.class)
+@ActiveProfiles("test")
+public class AuthControllerTest {
+
+    @MockBean(name = "AuthService")
+    private AuthService authService;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+    @Test
+
+    public void testPositiveLogin() throws Exception {
+
+        mockMvc.perform(post("/auth/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\n" +
+                    "    \"email\": \"testuser@gmail.com\",\n" +
+                    "    \"password\": \"password\"\n" +
+                    "}")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+    @Test
+    public void testPositiveNegativeBadUserName() throws Exception {
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "    \"email\": \"fakeuser@gmail.com\",\n" +
+                                "    \"password\": \"password\"\n" +
+                                "}")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void testPositiveNegativeBadPassword() throws Exception {
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "    \"email\": \"testuser@gmail.com\",\n" +
+                                "    \"password\": \"Bassword\"\n" +
+                                "}")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+
+}
