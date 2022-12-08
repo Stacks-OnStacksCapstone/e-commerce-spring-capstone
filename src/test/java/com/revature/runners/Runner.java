50013e7606ecc32a2a655975e0bf6e6024d79671
Brithnie Estime <brithnieestime@Brithnies-MacBook-Pro.local>
2022-12-07 19:55:07 -0500

    switched from testng to junit (new dependencies, updated runner file, new hooks file). updated userProfilePage. renamed and updated updateProfile class(updateProfileSteps).

--- src/test/java/com/revature/runners/Runner.java
+++ src/test/java/com/revature/runners/Runner.java
@@ -1,33 +1,9 @@
 package com.revature.runners;
 
-import com.revature.pages.UserProfilePage;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features", glue = "com.revature.stepsImplementation")
-public class Runner extends AbstractTestNGCucumberTests {
-
-    public static WebDriver driver;
-    public static WebDriverWait wait;
-    public static UserProfilePage userProfilePage;
-
-    @BeforeMethod
-    public void setUp() {
-        WebDriverManager.edgedriver().setup();
-        driver = new EdgeDriver();
-        wait = new WebDriverWait(driver, 20);
-        userProfilePage = new UserProfilePage(driver);
-    }
-
-    @AfterMethod
-    public void cleanUp() {
-        driver.quit();
-    }
-
-}
+public class Runner {}
