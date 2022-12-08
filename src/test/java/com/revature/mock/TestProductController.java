e445578f289453a83de9141118128658f68fadee
cmiller241 <99040885+cmiller241@users.noreply.github.com>
2022-12-08 09:59:00 -0500

    MockMVC Product Controller - all endpoints tested

--- src/test/java/com/revature/mock/TestProductController.java
+++ src/test/java/com/revature/mock/TestProductController.java
@@ -15,7 +15,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -46,5 +48,101 @@ public class TestProductController {
                 .andDo(print())
                 .andReturn();
     }
+
+    @Test
+    @DisplayName("Should List Product By ID - /api/product/{id}")
+    public void shouldListProductByID() throws Exception {
+
+        Product product = new Product(1,1,50,"test","img/src.gif","test",true);
+        when(productservice.findById(1)).thenReturn(Optional.of(product));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/product/1"))
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.description").value("test"))
+                .andExpect(jsonPath("$.image").value("img/src.gif"))
+                .andExpect(jsonPath("$.name").value("test"))
+                .andDo(print())
+                .andReturn();
+    }
+
+
+    @Test
+    @DisplayName("Should Delete Product By ID - /api/product/{id}")
+    public void shouldDeleteProductByID() throws Exception {
+
+        Product product = new Product(1,1,50,"test","img/src.gif","test",true);
+        when(productservice.findById(1)).thenReturn(Optional.of(product));
+        doNothing().when(productservice).delete(1);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/product/1"))
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.description").value("test"))
+                .andExpect(jsonPath("$.image").value("img/src.gif"))
+                .andExpect(jsonPath("$.name").value("test"))
+                .andDo(print())
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("Should Save Product By ID - /api/product/")
+    public void shouldSaveProductByID() throws Exception {
+
+        Product product1 = new Product(1,5,50,"Product LOL","test.gif","Skateboard",true);
+        Product product2 = new Product(0,1,50,"test2","img/src.gif","test2",true);
+        when(productservice.save(product1)).thenReturn(product2);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/product/")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content("{\"id\":\"1\",\"quantity\":\"5\",\"price\":\"50\",\"description\":\"Product LOL\",\"image\":\"test.gif\",\"name\":\"Skateboard\",\"isActive\":\"true\"}"))
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.description").value("test2"))
+                .andExpect(jsonPath("$.image").value("img/src.gif"))
+                .andExpect(jsonPath("$.name").value("test2"))
+                .andDo(print())
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("Should Search Product By Keyword - /api/product/search/")
+    public void shouldSearchProductByKeyword() throws Exception {
+
+        List<Product> product = new ArrayList<Product>();
+        product.add(new Product(1,5,50,"test2","test.gif","test2",true));
+        when(productservice.findByKeyword("cheese")).thenReturn(product);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/product/search/cheese"))
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$[0].description").value("test2"))
+                .andExpect(jsonPath("$[0].image").value("test.gif"))
+                .andExpect(jsonPath("$[0].name").value("test2"))
+                .andDo(print())
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("Should Edit Product Quantity By ID - /api/product/")
+    public void shouldEditProductQuantityByID() throws Exception {
+
+        Product product1 = new Product(1,5,50,"Product LOL","test.gif","Skateboard",true);
+        Product product2 = new Product(1,5,50,"test2","img/src.gif","test2",true);
+        when(productservice.findById(1)).thenReturn(Optional.of(product2));
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/api/product/")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content("[{\"id\":\"1\",\"quantity\":\"1\"}]"))
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$[0].description").value("test2"))
+                .andExpect(jsonPath("$[0].image").value("img/src.gif"))
+                .andExpect(jsonPath("$[0].name").value("test2"))
+                .andDo(print())
+                .andReturn();
+    }
+
+
 
 }
