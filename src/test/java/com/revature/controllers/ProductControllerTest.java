c2338ac93e817e6ffbc58aa190e8f842ff4b6c76
Joshua Gibbs <jgibbs9076@yahoo.com>
2022-12-08 22:45:26 -0500

    Updated

--- src/test/java/com/revature/controllers/ProductControllerTest.java
+++ src/test/java/com/revature/controllers/ProductControllerTest.java
@@ -1,192 +1,78 @@
 package com.revature.controllers;
 
-import com.revature.models.Product;
-import com.revature.services.ProductService;
+import com.revature.ECommerceApplication;
+import com.revature.services.AuthService;
+import com.revature.services.ProductReviewService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import java.util.List;
-import java.util.Optional;
-
-import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(ProductController.class)
+@SpringBootTest(classes = ECommerceApplication.class)
 public class ProductControllerTest {
-    @MockBean(name="ProductService")
-    private ProductService productService;
+    @MockBean(name = "AuthService")
+    private AuthService authService;
 
-    @Autowired private MockMvc mockMvc;
+    @MockBean(name = "ProductReviewService")
+    private ProductReviewService productReviewService;
 
-    // Test Collection of Products [headphone1, shirt2, mask, hat1, coat1]
-    // /api/product
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    // TODO: Add tests for getting all products
     @Test
     public void testGetAllProducts() throws Exception {
-        when(productService.findAll()).thenReturn(List.of(
-                // id, name, description, price, quantity, active
-                // headphone1
-                new Product(
-                1,
-                10,
-                20.0,
-                "A nice pair of headphones",
-                "https://i.insider.com/54eb437f6bb3f7697f85da71?width=1000&format=jpeg&auto=webp",
-                "Headphones",
-                true
-                ),
-                // shirt2
-                new Product(
-                7,
-                5,
-                99.99,
-                "A nicer MeeShirt",
-                "https://m.media-amazon.com/images/I/B1DBWbloIpS._CLa%7C2140%2C2000%7C51snuhwZFIL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_SL1500_.png",
-                "TeeShirt",
-                true
-                ),
-                // mask
-                new Product(
-                8,
-                20,
-                12.5,
-                "A reusable surgical mask",
-                "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2022_29/3512377/screen_shot_2021-10-13_at_2-00-29_pm.png",
-                "Mask",
-                true
-                ),
-                // hat1
-                new Product(
-                4,
-                20,
-                10.0,
-                "A fancy cap for a fancy person",
-                "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
-                "Hat",
-                true
-                ),
-                // coat1
-                new Product(
-                5,
-                3,
-                80.0,
-                "A nice coat",
-                "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
-                "Coat",
-                true
-                )
-        ));
-
-        this.mockMvc
-                .perform(MockMvcRequestBuilders.get("/api/product"))
+        mockMvc.perform(get("/api/products"))
                 .andDo(print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                // Check For Names of Products
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Headphones"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("TeeShirt"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[2].name").value("Mask"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[3].name").value("Hat"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[4].name").value("Coat")
-                );
+                .andExpect(status().isOk());
     }
 
-    // Test Finding Product By ID
-    // /api/product/{id}
+    // TODO: Add tests for getting a product by ID
     @Test
-    public void testProductById() throws Exception {
-        when(productService.findById(1)).thenReturn(Optional.of(
-                // headphone1
-                (new Product(
-                        1,
-                        10,
-                        20.0,
-                        "A nice pair of headphones",
-                        "https://i.insider.com/54eb437f6bb3f7697f85da71?width=1000&format=jpeg&auto=webp",
-                        "Headphones",
-                        true
-                ))
-        ));
-
-        this.mockMvc
-                .perform(MockMvcRequestBuilders.get("/api/product/1"))
+    public void testGetProductById() throws Exception {
+        mockMvc.perform(get("/api//products/{id}", 1))
                 .andDo(print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Headphones"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.description").value("A nice pair of headphones"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.price").value(20.0))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.quantity").value(10)
-                );
-
-        when(productService.findById(10)).thenReturn(Optional.of(
-                // headphone1
-                (new Product(
-                        10,
-                        3,
-                        1.99,
-                        "A niceish coat",
-                        "https://media.bergdorfgoodman.com/f_auto,q_auto:good,ar_5:7,c_fill,dpr_1.0,w_720/01/bg_4370145_100664_m",
-                        "Coat",
-                        true
-                ))
-        ));
-
-        this.mockMvc
-                .perform(MockMvcRequestBuilders.get("/api/product/10"))
-                .andDo(print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Coat"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.description").value("A niceish coat"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.price").value(1.99))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.quantity").value(3)
-                );
+                .andExpect(status().isOk());
     }
 
-    // Test Finding Product By Keyword
-    // /api/product/search/{keyword}
+    // TODO: Add tests for getting a product by name/keyword
     @Test
-    public void testProductByKeyword() throws Exception {
-        when(productService.findByKeyword("coat")).thenReturn(List.of(
-                // coat1
-                (new Product(
-                        5,
-                        3,
-                        80.0,
-                        "A nice coat",
-                        "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
-                        "Coat",
-                        true
-                )),
-                // coat2
-                (new Product(
-                        10,
-                        3,
-                        1.99,
-                        "A niceish coat",
-                        "https://media.bergdorfgoodman.com/f_auto,q_auto:good,ar_5:7,c_fill,dpr_1.0,w_720/01/bg_4370145_100664_m",
-                        "Coat",
-                        true
-                ))
-        ));
-
-        this.mockMvc
-                .perform(MockMvcRequestBuilders.get("/api/product/search/coat"))
+    public void testGetProductByKeyword() throws Exception {
+        mockMvc.perform(get("/api//products/{keyword}", "shirt"))
                 .andDo(print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Coat"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("Coat"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].description").value("A nice coat"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].description").value("A niceish coat"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].price").value(80.0))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].price").value(1.99))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].quantity").value(3))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].quantity").value(3)
-                );
+                .andExpect(status().isOk());
+    }
+
+    // TODO: Add tests for puttig a product
+    @Test
+    public void testPutProduct() throws Exception {
+        mockMvc.perform(get("/api//products/{id}", 1))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    // TODO: Add tests for deleting a product
+    @Test
+    public void testDeleteProduct() throws Exception {
+        mockMvc.perform(get("/api//products/{id}", 1))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
--- src/test/java/com/revature/controllers/ProductReviewControllerTest.java
+++ src/test/java/com/revature/controllers/ProductReviewControllerTest.java
@@ -16,10 +16,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringRunner.class)
@@ -61,7 +60,6 @@ public class ProductReviewControllerTest {
         int[] postIDs = {2, 1, 3, 4, 5, 6, 7, 8, 9, 10};
 
         for (int order : reviewOrder) {
-            System.out.println("Order: " + order + " Product ID: " + productIDs[order] + " Rating: " + ratings[order] + " Comment: " + comments[order] + " Post ID: " + postIDs[order]);
             this.mockMvc
                     .perform(get("/api/productreview"))
                     .andExpect(status().isOk())
@@ -74,18 +72,110 @@ public class ProductReviewControllerTest {
     }
 
     @Test
-    public void testGetProductByID() throws Exception {
-        int[] productIDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    public void testGetProductReviewByID() throws Exception {
+        // {productID, postID}
+        int[][] doubleIDs = {{1,2},{2,1},{3,3},{4,4},{5,5},{6,6},{7,7},{8,8},{9,9},{10,10}};
 
-        for (int productID : productIDs) {
-            System.out.println("Product ID: " + productID);
+        for (int[] doubleID : doubleIDs) {
             this.mockMvc
-                    .perform(get("/api/productreview/{id}")
-                    .param("id", String.valueOf(productID)))
+                    .perform(get("/api/productreview/{id}", doubleID[1]))
                     .andExpect(status().isOk())
-                    .andExpect(jsonPath(".id").value(productID))
+                    .andExpect(jsonPath("[0].id").value(doubleID[0]))
+                    .andExpect(jsonPath("[0].postId").value(doubleID[1]))
+                    .andDo(print());
+            }
+        }
+
+    @Test
+    public void testGetProductAverageRating() throws Exception {
+        // {productID, averageRating}
+        int[][] doublesAverages = {{1, 1},{2, 4},{3, 5},{4, 1},{5, 5},{6, 2},{7, 4},{8, 5},{9, 3},{10, 5}};
+
+        for (int[] doublesAverage : doublesAverages) {
+            this.mockMvc
+                    .perform(get("/api/productreview/avr/{id}", doublesAverage[0]))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(String.valueOf(doublesAverage[1])))
+                    .andDo(print());
+        }
+    }
+
+    // TODO: Add tests for checking if a user or admin is posting a review
+    @Test
+    public void testGetProductReviewCanPost() throws Exception {
+        // {postID, userID}
+        int[][] canPostIDs = {{2,1},{1,1},{3,2},{4,2},{5,2},{6,1},{7,1},{8,1},{9,2},{10,2}};
+
+        for (int[] canPostID : canPostIDs) {
+            if(canPostID[1] == 1) { // is admin
+                this.mockMvc
+                        .perform(get("/api/productreview/post/{post_id}/{user_id}", canPostID[0], canPostID[1]))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string("false"))
+                        .andDo(print());
+            } else if (canPostID[1] == 2) { // is user
+                this.mockMvc
+                        .perform(get("/api/productreview/post/{post_id}/{user_id}", canPostID[0], canPostID[1]))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string("true"))
+                        .andDo(print());
+            }
+        }
+    }
+
+    @Test
+    public void testGetProductReviewByIDAndRating() throws Exception {
+        // {rating, postID}
+        int[][] ratingIDs = {{4, 2}, {1, 1}, {5, 3}, {1, 4}, {5, 5}, {2, 6}, {4, 7}, {5, 8}, {3, 9}, {5, 10}};
+
+        for (int[] ratingID : ratingIDs) {
+            this.mockMvc
+                    .perform(get("/api/productreview/rate/{id}/{rating}", ratingID[1], ratingID[0]))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath(".rating").value(ratingID[0]))
+                    .andExpect(jsonPath(".postId").value(ratingID[1]))
+                    .andDo(print());
+        }
+    }
+
+    // TODO: Add test for posting product review
+    @Test
+    public void testPutProductReview() throws Exception {
+        // {id, rating, comment}
+        String[][] ratingComments = {
+                {"1", "4", "This shirt is the best"},
+                {"2", "1", "These sound like tin cans"},
+                {"3", "5", "Holds the things"},
+                {"4", "1", "Cool beans"},
+                {"5", "5", "A very nice coat"},
+                {"6", "2", "These sound like tin cans"},
+                {"7", "4", "A vote for me is a vote for this shirt"},
+                {"8", "5", "Air is the enemy"},
+                {"9", "3", "Nice feather"},
+                {"10", "5", "So puffy its like a cloud"}
+        };
+
+        for (String[] ratingComment : ratingComments) {
+            this.mockMvc
+                    .perform(put("/api/productreview/rate/{id}/{rating}", ratingComment[0], ratingComment[1]))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath(".rating").value(ratingComment[0]))
+                    .andExpect(jsonPath(".comment").value(ratingComment[1]))
+                    .andDo(print());
+        }
+    }
+
+    // TODO: Add test for deleting product review
+    @Test
+    public void testDeleteProductReview() throws Exception {
+        // {id}
+        int[] ids = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int id : ids) {
+            this.mockMvc
+                    .perform(delete("/api/productreview/{id}", id))
+                    .andExpect(status().isOk())
                     .andDo(print());
         }
     }
 }
-
