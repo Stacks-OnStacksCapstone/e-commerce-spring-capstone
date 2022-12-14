bde32edda5b1572c0d684db345e07fa7b95b3594
Joshua Gibbs <jgibbs9076@yahoo.com>
2022-12-14 08:56:24 -0500

    Updated

--- src/test/java/com/revature/controllers/ProductControllerTest.java
+++ src/test/java/com/revature/controllers/ProductControllerTest.java
@@ -17,7 +17,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -51,13 +50,11 @@ public class ProductControllerTest {
                         .andExpect(status().isOk())
                         .andReturn();
 
-        String token = result.getResponse().getHeader("Authorization");
-
-        return token;
+        return result.getResponse().getHeader("Authorization");
     }
 
     @Test
-    public void testPatchProduct() throws Exception {
+    public void testPatchProductPositive() throws Exception {
         this.mockMvc
                 .perform(patch("/api/product")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -75,7 +72,39 @@ public class ProductControllerTest {
     }
 
     @Test
-    public void testPutProduct() throws Exception {
+    public void testPatchProductNegative1() throws Exception {
+        this.mockMvc
+                .perform(patch("/api/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("[\n" +
+                                "    {\n" +
+                                "        \"id\": 9999,\n" +
+                                "        \"quantity\": 9999\n" +
+                                "    }\n" +
+                                "  ]")
+                        .header("Authorization", getToken()))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    public void testPatchProductNegative2() throws Exception {
+        this.mockMvc
+                .perform(patch("/api/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("[\n" +
+                                "    {\n" +
+                                "        \"id\": 3,\n" +
+                                "        \"quantity\": 9999\n" +
+                                "    }\n" +
+                                "  ]")
+                        .header("Authorization", getToken()))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    public void testPutProductPositive() throws Exception {
         this.mockMvc
                 .perform(put("/api/product")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -101,111 +130,22 @@ public class ProductControllerTest {
     }
 
     @Test
-    public void testGetAllProducts() throws Exception {
-        int[] orders = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-        int[] productIDs = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] quantity = {10, 5, 16, 20, 3, 10, 5, 20, 1};
-        double[] price = {20.0, 45.0, 2.5, 10.0, 80.0, 120.0, 99.99, 12.5, 1000.0};
-        String[] descriptions = {
-                "A nice pair of headphones",
-                "A nice TeeShirt",
-                "A reusable shopping bag",
-                "A fancy cap for a fancy person",
-                "A nice coat",
-                "A beater set of headphones",
-                "A nicer MeeShirt",
-                "A reusable surgical mask",
-                "A fancier hat for a fancier person"
-        };
-        String[] images = {
-                "https://i.insider.com/54eb437f6bb3f7697f85da71?width=1000&format=jpeg&auto=webp",
-                "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
-                "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
-                "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
-                "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
-                "https://www.beatsbydre.com/content/dam/beats-support/global/images/product-hero/set-up-and-use-studio-wireless.jpg.large.2x.jpg",
-                "https://m.media-amazon.com/images/I/B1DBWbloIpS._CLa%7C2140%2C2000%7C51snuhwZFIL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_SL1500_.png",
-                "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2022_29/3512377/screen_shot_2021-10-13_at_2-00-29_pm.png",
-                "https://cdn.shopify.com/s/files/1/2624/7744/products/ReversibleRen-RedSilver-Leather-TopHat-AmericanHatMakers-Men-STUD_900x900.jpg?v=1668713281"
-        };
-        String[] names = {
-                "Headphones",
-                "TeeShirt",
-                "Shopping Bag",
-                "Hat",
-                "Coat",
-                "Headphones",
-                "TeeShirt",
-                "Mask",
-                "Hat"
-        };
-
-        for(int order : orders) {
-            this.mockMvc
-                    .perform(get("/api/product"))
-                    .andDo(print())
-                    .andExpect(MockMvcResultMatchers.status().isOk())
-                    // Check For Names of Products
-                    .andExpect(MockMvcResultMatchers.jsonPath("[" + order + "].id").value(productIDs[order]))
-                    .andExpect(MockMvcResultMatchers.jsonPath("[" + order + "].quantity").value(quantity[order]))
-                    .andExpect(MockMvcResultMatchers.jsonPath("[" + order + "].price").value(price[order]))
-                    .andExpect(MockMvcResultMatchers.jsonPath("[" + order + "].description").value(descriptions[order]))
-                    .andExpect(MockMvcResultMatchers.jsonPath("[" + order + "].image").value(images[order]))
-                    .andExpect(MockMvcResultMatchers.jsonPath("[" + order + "].name").value(names[order]))
-                    .andExpect(MockMvcResultMatchers.jsonPath("[" + order + "].active").value(true));
-        }
-    }
-
-    @Test
-    public void testDeleteProduct() throws Exception {
+    public void testPutProductNegative() throws Exception {
         this.mockMvc
-                .perform(delete("/api/product/{id}", 10)
-                .header("Authorization", getToken()))
+                .perform(put("/api/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{   \n" +
+                                "    \"id\": 0,\n" +
+                                "    \"quantity\": 0,\n" +
+                                "    \"price\": 0,\n" +
+                                "    \"description\": \"Air\",\n" +
+                                "    \"image\": \"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.swXJc8G6YJMcaO692mCoVAHaC1%26pid%3DApi&f=1&ipt=1e48299a2bf5e222bde47be1c36e0fedd2ed818df68879bedf58acc9defbc89e&ipo=images\",\n" +
+                                "    \"name\": \"Air\",\n" +
+                                "    \"active\": false\n" +
+                                "}")
+                        .header("Authorization", getToken()))
                 .andDo(print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath(".id").value(10))
-                .andExpect(MockMvcResultMatchers.jsonPath(".quantity").value(3))
-                .andExpect(MockMvcResultMatchers.jsonPath(".price").value(1.99))
-                .andExpect(MockMvcResultMatchers.jsonPath(".description").value("A niceish coat"))
-                .andExpect(MockMvcResultMatchers.jsonPath(".image").value("https://media.bergdorfgoodman.com/f_auto,q_auto:good,ar_5:7,c_fill,dpr_1.0,w_720/01/bg_4370145_100664_m"))
-                .andExpect(MockMvcResultMatchers.jsonPath(".name").value("Coat"))
-                .andExpect(MockMvcResultMatchers.jsonPath(".active").value(false));
-    }
-
-    @Test
-    public void testProductById() throws Exception {
-        int[] orders = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-        String[] names = {
-                "Headphones",
-                "TeeShirt",
-                "Shopping Bag",
-                "Hat",
-                "Coat",
-                "Headphones",
-                "TeeShirt",
-                "Mask",
-                "Hat"
-        };
-        String[] descriptions = {
-                "A nice pair of headphones",
-                "A nice TeeShirt",
-                "A reusable shopping bag",
-                "A fancy cap for a fancy person",
-                "A nice coat",
-                "A beater set of headphones",
-                "A nicer MeeShirt",
-                "A reusable surgical mask",
-                "A fancier hat for a fancier person"
-        };
-
-        for(int order : orders) {
-            this.mockMvc
-                    .perform(get("/api/product/{id}", order + 1))
-                    .andDo(print())
-                    .andExpect(MockMvcResultMatchers.status().isOk())
-                    .andExpect(MockMvcResultMatchers.jsonPath(".name").value(names[order]))
-                    .andExpect(MockMvcResultMatchers.jsonPath(".description").value(descriptions[order]));
-        }
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 
     @Test
@@ -227,5 +167,125 @@ public class ProductControllerTest {
                     .andExpect(MockMvcResultMatchers.jsonPath("[0].name").value(product[1]))
                     .andExpect(MockMvcResultMatchers.jsonPath("[0].description").value(product[2]));
         }
+    }
+
+    @Test
+    public void testDeleteProductPositive() throws Exception {
+        this.mockMvc
+                .perform(delete("/api/product/{id}", 9)
+                        .header("Authorization", getToken()))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath(".id").value(9))
+                .andExpect(MockMvcResultMatchers.jsonPath(".quantity").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath(".price").value(1000.0))
+                .andExpect(MockMvcResultMatchers.jsonPath(".description").value("A fancier hat for a fancier person"))
+                .andExpect(MockMvcResultMatchers.jsonPath(".image").value("https://cdn.shopify.com/s/files/1/2624/7744/products/ReversibleRen-RedSilver-Leather-TopHat-AmericanHatMakers-Men-STUD_900x900.jpg?v=1668713281"))
+                .andExpect(MockMvcResultMatchers.jsonPath(".name").value("Hat"))
+                .andExpect(MockMvcResultMatchers.jsonPath(".active").value(false));
+    }
+
+    @Test
+    public void testDeleteProductNegative() throws Exception {
+        this.mockMvc
+                .perform(delete("/api/product/{id}", 9999)
+                        .header("Authorization", getToken()))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    public void testGetAllProducts() throws Exception {
+        int[] orders = {0, 1, 2, 3, 4, 5, 6, 7};
+        int[] productIDs = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] quantity = {10, 5, 16, 20, 3, 10, 5, 20};
+        double[] price = {20.0, 45.0, 2.5, 10.0, 80.0, 120.0, 99.99, 12.5};
+        String[] descriptions = {
+                "A nice pair of headphones",
+                "A nice TeeShirt",
+                "A reusable shopping bag",
+                "A fancy cap for a fancy person",
+                "A nice coat",
+                "A beater set of headphones",
+                "A nicer MeeShirt",
+                "A reusable surgical mask",
+        };
+        String[] images = {
+                "https://i.insider.com/54eb437f6bb3f7697f85da71?width=1000&format=jpeg&auto=webp",
+                "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+                "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+                "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+                "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+                "https://www.beatsbydre.com/content/dam/beats-support/global/images/product-hero/set-up-and-use-studio-wireless.jpg.large.2x.jpg",
+                "https://m.media-amazon.com/images/I/B1DBWbloIpS._CLa%7C2140%2C2000%7C51snuhwZFIL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_SL1500_.png",
+                "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2022_29/3512377/screen_shot_2021-10-13_at_2-00-29_pm.png",
+        };
+        String[] names = {
+                "Headphones",
+                "TeeShirt",
+                "Shopping Bag",
+                "Hat",
+                "Coat",
+                "Headphones",
+                "TeeShirt",
+                "Mask",
+        };
+
+        for(int order : orders) {
+            this.mockMvc
+                    .perform(get("/api/product"))
+                    .andDo(print())
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    // Check For Names of Products
+                    .andExpect(MockMvcResultMatchers.jsonPath("[" + order + "].id").value(productIDs[order]))
+                    .andExpect(MockMvcResultMatchers.jsonPath("[" + order + "].quantity").value(quantity[order]))
+                    .andExpect(MockMvcResultMatchers.jsonPath("[" + order + "].price").value(price[order]))
+                    .andExpect(MockMvcResultMatchers.jsonPath("[" + order + "].description").value(descriptions[order]))
+                    .andExpect(MockMvcResultMatchers.jsonPath("[" + order + "].image").value(images[order]))
+                    .andExpect(MockMvcResultMatchers.jsonPath("[" + order + "].name").value(names[order]))
+                    .andExpect(MockMvcResultMatchers.jsonPath("[" + order + "].active").value(true));
+        }
+    }
+
+    @Test
+    public void testProductByIdPositive() throws Exception {
+        int[] orders = {0, 1, 2, 3, 4, 5, 6, 7};
+        String[] names = {
+                "Headphones",
+                "TeeShirt",
+                "Shopping Bag",
+                "Hat",
+                "Coat",
+                "Headphones",
+                "TeeShirt",
+                "Mask",
+        };
+        String[] descriptions = {
+                "A nice pair of headphones",
+                "A nice TeeShirt",
+                "A reusable shopping bag",
+                "A fancy cap for a fancy person",
+                "A nice coat",
+                "A beater set of headphones",
+                "A nicer MeeShirt",
+                "A reusable surgical mask",
+        };
+
+        for(int order : orders) {
+            this.mockMvc
+                    .perform(get("/api/product/{id}", order + 1))
+                    .andDo(print())
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.jsonPath(".name").value(names[order]))
+                    .andExpect(MockMvcResultMatchers.jsonPath(".description").value(descriptions[order]));
+        }
+    }
+
+    @Test
+    public void testProductByIdNegative() throws Exception {
+        this.mockMvc
+                .perform(get("/api/product/{id}", 100))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 }
