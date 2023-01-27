c660a06f34a61716a7f72ab87b24919bcb5b7660
Skye Gibney <skyejgibney@gmail.com>
2023-01-27 10:02:31 -0800

    Modified populateTables.sql to avoid ID conflicts

--- src/main/resources/populateTables.sql
+++ src/main/resources/populateTables.sql
@@ -6,42 +6,50 @@ truncate table product_review cascade;
 truncate table order_details cascade;
 
 insert into users
-values (1, 'tets@gmail.com', 'test', true, false, 'users', '!test123', 'token'),
-	(2, 'mickeymouse@example.com', 'Mickey', true, false, 'Mouse', 'pass123', 'token'),
-	(3, 'tonythetiger@example.com', 'Tony', false, false, 'Tiger', 'pass123', 'token'),
-	(4, 'wirtualtm@example.com', 'Wirtual', false, true, 'TM', 'pass123', 'token'),
-	(5, 'nameynamenson@example.com', 'Namey', true, true, 'Namenson', 'pass123', 'token');
+values (1000000, 'tets@gmail.com', 'test', true, false, 'users', '!test123', 'token'),
+	(1000001, 'mickeymouse@example.com', 'Mickey', true, false, 'Mouse', 'pass123', 'token'),
+	(1000002, 'tonythetiger@example.com', 'Tony', false, false, 'Tiger', 'pass123', 'token'),
+	(1000003, 'wirtualtm@example.com', 'Wirtual', false, true, 'TM', 'pass123', 'token'),
+	(1000004, 'nameynamenson@example.com', 'Namey', true, true, 'Namenson', 'pass123', 'token');
 
 insert into user_payments
-values ('CC1', '1111-2222-3333-4444', '123', '1-1-2030', 1),
-	('CC2', '1111-2222-3333-4444', '123', '1-1-2030', 1),
-	('CC3', '1111-2222-3333-4444', '123', '1-1-2030', 2),
-	('CC4', '1111-2222-3333-4444', '123', '1-1-2030', 3);
+values ('CC1', '1111-2222-3333-4444', '123', '1-1-2030', 1000000),
+	('CC2', '1111-2222-3333-4444', '123', '1-1-2030', 1000000),
+	('CC3', '1111-2222-3333-4444', '123', '1-1-2030', 1000001),
+	('CC4', '1111-2222-3333-4444', '123', '1-1-2030', 1000002),
+	('CC5', '1111-2222-3333-4444', '123', '1-1-2030', 1000003),
+	('CC6', '1111-2222-3333-4444', '123', '1-1-2030', 1000004);
 
 insert into product
-values (1, 'this product is  an apple', 'none', true, 'apple', 2, 15),
-	(2, 'this product is  an orange', 'none', true, 'orange', 1, 20),
-	(3, 'this product is  a pineapple', 'none', true, 'pineapple', 4, 25),
-	(4, 'See and speak at the same time!', '', true, 'Microphone Lamp', 10.99, 10),
-	(5, 'Spice up any bookshelp with this keepsake!', '', false, 'Lightning McQueen Model', 3.99, 0),
-	(6, 'Keep ahead of fashion by taking these wherever you go!', '', true, 'Shrek Crocs', 5.99, 1);
+values (1000000, 'this product is  an apple', 'https://cdn.pixabay.com/photo/2014/02/01/17/28/apple-256261_960_720.jpg', true, 'apple', 2, 15),
+	(1000001, 'this product is  an orange', 'https://cdn.pixabay.com/photo/2017/12/29/16/34/fruit-3048001_960_720.jpg', true, 'orange', 1, 20),
+	(1000002, 'this product is  a pineapple', 'https://cdn.pixabay.com/photo/2015/02/14/18/10/pineapple-636562_960_720.jpg', true, 'pineapple', 4, 25),
+	(1000003, 'See and speak at the same time!', 'https://cdn.pixabay.com/photo/2017/10/30/23/34/lamp-2903830_960_720.jpg', true, 'Microphone Lamp', 10.99, 10),
+	(1000004, 'Spice up any bookshelp with this keepsake!', 'https://cdn.pixabay.com/photo/2017/04/16/16/52/lightning-mcqueen-2235210_960_720.jpg', false, 'Lightning McQueen Model', 3.99, 20),
+	(1000005, 'Keep ahead of fashion by taking these wherever you go!', 'https://cdn.pixabay.com/photo/2015/04/14/17/08/alien-722415_960_720.jpg', true, 'Shrek Crocs', 5.99, 1);
 
 insert into orders
-values (1, '1-1-2023', 'Example Address', 'CC1', 1),
-	(2, '1-1-2023', 'Example Address', 'CC2', 1),
-	(3, '1-1-2023', 'Example Address', 'CC3', 2),
-	(4, '1-1-2023', 'Example Address', 'CC4', 3);
+values (1000000, '1-1-2023', 'Example Address', 'CC1', 1000000),
+	(1000001, '1-1-2023', 'Example Address', 'CC2', 1000000),
+	(1000002, '1-1-2023', 'Example Address', 'CC3', 1000001),
+	(1000003, '1-1-2023', 'Example Address', 'CC4', 1000002),
+	(1000004, '1-1-2023', 'Example Address', 'CC5', 1000003),
+	(1000005, '1-1-2023', 'Example Address', 'CC6', 1000004);
 
-insert into order_details 
-values (1, 2, 1, 1),
-	(2, 3, 2, 2),
-	(3, 20, 3, 3),
-	(4, 4, 4, 1);
-	
+insert into order_details
+values (1000000, 2, 1000000, 1000003),
+	(1000001, 3, 1000000, 1000005),
+	(1000002, 20, 1000001, 1000002),
+	(1000003, 1, 1000001, 1000005),
+	(1000004, 1, 1000002, 1000000),
+	(1000005, 1, 1000003, 1000000),
+	(1000006, 1, 1000004, 1000000),
+	(1000007, 1, 1000005, 1000000);
 
-insert into product_review (comment, rating, product_id, user_id) values ('the apples had brown spots and were over ripe.', 1, 1, 2);
-insert into product_review (comment, rating, product_id, user_id) values ('the oranges had so much juice!', 4, 2, 1);
-
-
-	
-
+insert into product_review
+values (1000000, 'the apples had brown spots and were over ripe.', 1, 1000000, 1000001),
+	(1000001, 'the oranges had so much juice!', 4, 1000001, 1000000),
+	(1000002, 'Pineapples are the best of both the apple and pine worlds!', 5, 1000002, 1000000),
+	(1000003, 'Wow! I didn''t know that I needed this!', 5, 1000003, 1000002),
+	(1000004, 'I love cars!', 5, 1000004, 1000003),
+	(1000005, 'These crocs are rad!', 5, 1000005, 1000004);
